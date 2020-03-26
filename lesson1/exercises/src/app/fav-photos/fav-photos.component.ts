530b1503fc3ebef9717ad0ffe779656665373b40
nreppb <nadiasani27@gmail.com>
2020-03-26 15:54:52 -0500

    Adding Lesson1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.redd.it/gz46n32em0p41.jpg';
+  image3 = 'https://external-preview.redd.it/ZPEvUNrnEIgIx5zfADEIpwsZx4bBIAslfExQC1iNIWI.jpg?auto=webp&s=bbed883eb3c8771f47b101203da38904d0f09b61';
 
   constructor() { }
 
